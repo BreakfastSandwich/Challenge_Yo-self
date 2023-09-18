a1310f2629096f70deda3f6dea6b23ea37cb3b9d
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-09-18 10:07:36 -0400

    morning commit, some JS updates

--- assets.css/script.js
+++ assets.css/script.js
@@ -12,7 +12,8 @@
 
 // when each question/object is presented the following should happen
     // the questions should replace the text of id="question"
-    // the answer choices should replace id= "a1, a2, a3, & a4"
+    // the answer choices should create an LI, add answer text, add class "answer"
+        // then append to "answer-section" ul
     // once a choice is selected a "correct" or "incorrect" is displayed in id="verdict"
     // if correct: increase score, if incorrect: decrease timmer
 
@@ -37,3 +38,59 @@
     // button to view scoreboard
     // quiz title
     // current timer
+
+
+
+    var resetScoresBtnEl = document.getElementById('reset-btn')
+    var playAgainBtnEl = document.getElementById('play-again-btn')
+    var quizSectionEl = document.getElementById('quizQuestions')
+    var scoreboardEl = document.getElementById('scoreboard')
+    var viewScoreEl = document.getElementById('viewScore')
+    var questionEl = document.getElementById('question')
+    var verdictEl = document.getElementById('verdict')
+    var introEl = document.getElementById('intro');
+    var scoreEl = document.getElementById('score')
+    var timerEl = document.getElementById('timer')
+    var nextBtnEl = document.getElementById('next-btn')
+    var startBtnEl = document.getElementById('start-btn')
+    var answersUlEl = document.getElementsByClassName('answer-section')
+    
+    
+    
+    var score = 0
+    var secondsLeft = 60
+
+    function countDownTimer () {
+
+        var timerInterval = setInterval(function() {
+            secondsLeft--;
+            timerEl.textContent = secondsLeft +"s"
+
+            if( secondsLeft === 0 ){
+            timerEl.textContent = "Out of time!"
+            secondsLeft
+        
+            endGame()
+            }
+        }, 100)
+    };
+
+    
+function startQuiz() {
+
+introEl.setAttribute('class', 'hide')
+quizSectionEl.setAttribute('class', 'show')
+scoreboardEl.setAttribute('class', 'hide')
+countDownTimer()
+};
+
+startBtnEl.addEventListener('click', startQuiz) ;   
+
+
+function wrongAnswer() {
+    secondsLeft = secondsLeft - 10
+}
+
+function correctAnswer() {
+    score = score + 10
+}
