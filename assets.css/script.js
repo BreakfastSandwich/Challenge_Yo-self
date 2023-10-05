14c471d657936b0ac2ac188bd47c1309853e988e
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-10-05 10:32:02 -0400

    updated JS, saved to alt repo by mistake.

--- assets.css/script.js
+++ assets.css/script.js
@@ -65,119 +65,134 @@ var answersEl = document.querySelectorAll('.answer')
 
 
 
-var question1 = {
-    question: "What is the coding languge that adds style to a webpage?",
-    answer: [
-        {
-            potentalAnswer: "CSS",
-            correct: true,
-        },
-        {
-            potentalAnswer: "HTML",
-            correct: false,
-        },
-        {
-            potentalAnswer: "JavaScript",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Disco",
-            correct: false,
-        }
-    ]
+
+function wrongAnswer() {
+    secondsLeft = secondsLeft - 10
+    verdictEl.textContent = "Inorrect!"
+    questionNumber++
 }
 
-var question2 = {
-    question: "what is the best flavor of Ice Cream?",
-    answer: [
-        {
-            potentalAnswer: "Cookies and Cream",
-            correct: true,
-        },
-        {
-            potentalAnswer: "Rocky Roads",
-            correct: false,
-        },
-        {
-            potentalAnswer: "MooseTracks",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Vanilla",
-            correct: false,
-        }
-    ]
-}
-
-var question3 = {
-    question: "What animal makes the best pet?",
-    answer: [
-        {
-            potentalAnswer: "Dog",
-            correct: true,
-        },
-        {
-            potentalAnswer: "Cat",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Black Bear",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Moose",
-            correct: false,
-        }
-    ]
-}
-
-var question4 = {
-    question: "What is the best Gaming Console",
-    answer: [
-        {
-            potentalAnswer: "PC",
-            correct: true,
-        },
-        {
-            potentalAnswer: "XBox",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Playstation",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Nintendo Switch",
-            correct: false,
-        }
-    ]
-}
-
-var question5 = {
-    question: "What is the best season?",
-    answer: [
-        {
-            potentalAnswer: "Winter",
-            correct: true,
-        },
-        {
-            potentalAnswer: "Summer",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Fall",
-            correct: false,
-        },
-        {
-            potentalAnswer: "Spring",
-            correct: false,
-        }
-    ]
+function correctAnswer() {
+    score = score + 10
+    verdictEl.setAttribute('class', 'show')
+    verdictEl.textContent = "Correct!"
+    questionNumber++
 }
 
 
-var questionList = [question1,question2,question3,question4, question5]
 
+
+var questionArray = [
+    {
+        question: "What is the coding languge that adds style to a webpage?",
+        answer: [
+            {
+                potentalAnswer: "CSS",
+                correct: true,
+            },
+            {
+                potentalAnswer: "HTML",
+                correct: false,
+            },
+            {
+                potentalAnswer: "JavaScript",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Disco",
+                correct: false,
+            }
+        ]
+    },
+    {
+        question: "what is the best flavor of Ice Cream?",
+        answer: [
+            {
+                potentalAnswer: "Cookies and Cream",
+                correct: true,
+            },
+            {
+                potentalAnswer: "Rocky Roads",
+                correct: false,
+            },
+            {
+                potentalAnswer: "MooseTracks",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Vanilla",
+                correct: false,
+            }
+        ]
+    },
+    {
+        question: "What animal makes the best pet?",
+        answer: [
+            {
+                potentalAnswer: "Dog",
+                correct: true,
+            },
+            {
+                potentalAnswer: "Cat",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Black Bear",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Moose",
+                correct: false,
+            }
+        ]
+    },
+    {
+        question: "What is the best Gaming Console",
+        answer: [
+            {
+                potentalAnswer: "PC",
+                correct: true,
+            },
+            {
+                potentalAnswer: "XBox",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Playstation",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Nintendo Switch",
+                correct: false,
+            }
+        ]
+    },
+    {
+        question: "What is the best season?",
+        answer: [
+            {
+                potentalAnswer: "Winter",
+                correct: true,
+            },
+            {
+                potentalAnswer: "Summer",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Fall",
+                correct: false,
+            },
+            {
+                potentalAnswer: "Spring",
+                correct: false,
+            }
+        ]
+    }]
+
+
+// var questionList = [question1, question2, question3, question4, question5]
+var question = questionArray[questionNumber].question
+var answers = questionArray[questionNumber].answer
 
 var score = 0
 var secondsLeft = 60
@@ -194,51 +209,67 @@ function countDownTimer() {
 
             endGame()
         }
-    }, 1000)
+    }, 100)
 };
 
-function submitQ(){
-    if (correct){
-    correctAnswer()
-} else if(!correct){
-    wrongAnswer
-}}
+
+function submitQ() {
+
+    console.log("tacos")
+    if (answer === true && secondsLeft >=0) {
+        correctAnswer()
+    } else if (answer === false || secondsLeft <=0 ) {
+        wrongAnswer
+    }
+}
 
 function startQuiz() {
 
     introEl.setAttribute('class', 'hide')
     quizSectionEl.setAttribute('class', 'show')
     scoreboardEl.setAttribute('class', 'hide')
+    verdictEl.setAttribute('class', 'hide')
     countDownTimer()
 
 
 
-    var question = questionList[questionNumber].question
-    var answers = questionList[questionNumber].answer
-    console.log(answers)
+
+
     var eventEl = event.target
 
     questionEl.textContent = question
     a1El.textContent = answers[0].potentalAnswer
+    let a1 = answers[0].correct
+    a1El.setAttribute('data-correct', a1)
+    let a1ElAtt = a1El.getAttribute('data-correct')
+    console.log(answers, a1)
+    let a2 = answers[1].correct
     a2El.textContent = answers[1].potentalAnswer
-    a3El.textContent = answers[2].potentalAnswer
-    a4El.textContent = answers[3].potentalAnswer
+    a2El.setAttribute('data-correct', a2)
 
-    function wrongAnswer() {
-        secondsLeft = secondsLeft - 10
-        verdictEl.textContent = "Inorrect!"
-    }
-    
-    function correctAnswer() {
-        score = score + 10
-        verdictEl.textContent = "Correct!"
-    }
-    eventEl.addEventListener('click', function test(){
+    let a3 = answers[2].correct
+    a3El.textContent = answers[2].potentalAnswer
+    a3El.setAttribute('data-correct', a3)
+
+    let a4 = answers[3].correct
+    a4El.textContent = answers[3].potentalAnswer
+    a4El.setAttribute('data-correct', a4)
+
+   
+    eventEl.addEventListener('click', function test() {
         console.log("taco")
 
     })
-    
- 
+
+    verdictEl.setAttribute('class', 'show')
+    verdictEl.textContent = "Correct!"
+
+
+    a1El.addEventListener('click', submitQ)
+
+
+
+
 
     // var answers = questionList[questionNumber].potentalAnswer
     // console.log(answers)
@@ -254,10 +285,10 @@ function startQuiz() {
     //     if (
 
     //     )
-        
 
 
- 
+
+
 
 
 
@@ -271,13 +302,13 @@ function startQuiz() {
 startBtnEl.addEventListener('click', startQuiz);
 
 
-function wrongAnswer() {
-    secondsLeft = secondsLeft - 10
-    verdictEl.textContent = "Inorrect!"
-}
 
-function correctAnswer() {
-    score = score + 10
-    verdictEl.textContent = "Correct!"
-}
 
+
+function endGame() {
+    introEl.setAttribute('class', 'hide')
+    quizSectionEl.setAttribute('class', 'hide')
+    scoreboardEl.setAttribute('class', 'show')
+    verdictEl.setAttribute('class', 'hide')
+    enterScore()
+}
